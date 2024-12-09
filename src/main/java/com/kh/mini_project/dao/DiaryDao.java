24a4fcaff5734147cf1fc4314fcf743a5962322c
Lee Sangwon <83111797+swon2id@users.noreply.github.com>
2024-12-09 15:21:35 +0900

    Revert "home추가"
    This reverts commit a5357265542eb3dd6415c1493ac6c3f3b1edecfe.

--- src/main/java/com/kh/mini_project/dao/DiaryDao.java
+++ src/main/java/com/kh/mini_project/dao/DiaryDao.java
@@ -2,24 +2,18 @@ package com.kh.mini_project.dao;
 
 import com.kh.mini_project.vo.DiaryVo;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.List;
 
 import static com.kh.mini_project.common.DiaryQuery.*;
 
-@Slf4j
 @Repository
 @RequiredArgsConstructor
 public class DiaryDao {
@@ -83,37 +77,5 @@ public class DiaryDao {
 
     public void deleteByDiaryNum(int diaryNum) {
         jdbcTemplate.update("DELETE FROM DIARY WHERE DIARY_NUM = ?", diaryNum);
-    }
-
-    public static final String SELECT_ALL_DIARY = "" +
-            "SELECT * FROM diary " +
-            "WHERE member_num = (" +
-            "   SELECT member_num FROM member " +
-            "   WHERE id = ?) " +
-            "ORDER BY written_date ASC";
-
-    // 해당 회원의 일기 전체 조회
-    public List<DiaryVo> diaryListByMember(String userId) {
-        try {
-            return jdbcTemplate.query((SELECT_ALL_DIARY), new Object[]{userId}, new DiaryRowMapper());
-        } catch (DataAccessException e) {
-            log.error("다이어리 조회 중 에러 발생. ", e);
-            throw e;
-        }
-    }
-
-    // Row Mapper
-    private static class DiaryRowMapper implements RowMapper<DiaryVo> {
-
-        @Override
-        public DiaryVo mapRow(ResultSet rs, int rowNum) throws SQLException {
-            return new DiaryVo(
-                    rs.getInt("diary_num"),
-                    rs.getInt("member_num"),
-                    rs.getString("title"),
-                    rs.getString("content"),
-                    rs.getTimestamp("written_date").toLocalDateTime()
-            );
-        }
     }
 }
